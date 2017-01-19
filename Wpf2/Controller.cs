dd35d39ea3a53c2889e224f603eec4bad272c784
jimmi <jimmi@DESKTOP-2OTI0T5>
2017-01-19 12:14:55 +0100

    Add project files.

---
+++ Wpf2/Controller.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf2
+{
+    class Controller
+    {
+        private static Controller instance;
+        private Repository repository;
+        public Person CurrentPerson { get; private set; }
+        public int PersonCount { get; private set; }
+        public int PersonIndex { get; private set; }
+
+        private Controller()
+        {
+            CurrentPerson = null;
+            repository = new Repository();
+            PersonCount = 0;
+            PersonIndex = -1;
+        }
+        public static Controller GetInstance()
+        {
+            if (instance== null)
+            {
+                instance = new Controller();
+            }
+            return instance;
+        }
+
+        public void AddPerson()
+        {
+            Person person = new Person();
+            CurrentPerson = person;
+            repository.AddPerson(person);
+            PersonCount = repository.PersonList.Count();
+            PersonIndex = PersonCount-1;
+        }
+
+        public void DeletePerson()
+        {
+            if (CurrentPerson != null)
+            {
+                repository.RemovePerson(CurrentPerson);
+                PersonIndex--;
+                PersonCount = repository.PersonList.Count();
+                CurrentPerson = repository.GetPersonAtIndex(PersonIndex);
+            }
+        }
+
+        public void NextPerson()
+        {
+            if (PersonIndex < PersonCount - 1)
+            {
+                PersonIndex++;
+                CurrentPerson = repository.GetPersonAtIndex(PersonIndex);
+            }
+        }
+        public void PrevPerson()
+        {
+            if (PersonIndex > 0)
+            {
+                PersonIndex--;
+                CurrentPerson = repository.GetPersonAtIndex(PersonIndex);
+            }
+        }
+
+
+    }
+}
---
+++ Wpf2/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf2
+{
+    public partial class MainWindow : Window
+    {
+        Controller controller;
+        public MainWindow()
+        {
+            controller = Controller.GetInstance();
+            InitializeComponent();
+
+            countLabel.Content = controller.PersonCount;
+            countIndex.Content = controller.PersonIndex;
+
+            newBtn.IsEnabled = false;
+        }
+
+        private void SetButton()
+        {
+            newBtn.IsEnabled = (firstNameTxtbox.Text != "" && lastNameTxtbox.Text != "" && ageTxtbox.Text != "" && numberTxtbox.Text != "");
+        }
+        private void newBtn_Click(object sender, RoutedEventArgs e)
+        {
+            controller.AddPerson();
+            controller.CurrentPerson.FirstName = firstNameTxtbox.Text;
+            controller.CurrentPerson.LastName = lastNameTxtbox.Text;
+            controller.CurrentPerson.Age = int.Parse(ageTxtbox.Text);
+            controller.CurrentPerson.TelephoneNr = numberTxtbox.Text;
+
+            firstNameTxtbox.Clear();
+            lastNameTxtbox.Clear();
+            ageTxtbox.Clear();
+            numberTxtbox.Clear();
+
+            countLabel.Content = controller.PersonCount;
+            countIndex.Content = controller.PersonIndex;
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            controller.DeletePerson();
+
+            firstNameTxtbox.Clear();
+            lastNameTxtbox.Clear();
+            ageTxtbox.Clear();
+            numberTxtbox.Clear();
+
+            countLabel.Content = controller.PersonCount;
+            countIndex.Content = controller.PersonIndex;
+        }
+
+        private void upBtn_Click(object sender, RoutedEventArgs e)
+        {
+            controller.NextPerson();
+
+            firstNameTxtbox.Text = controller.CurrentPerson.FirstName;
+            lastNameTxtbox.Text = controller.CurrentPerson.LastName;
+            ageTxtbox.Text = controller.CurrentPerson.Age.ToString();
+            numberTxtbox.Text = controller.CurrentPerson.TelephoneNr;
+
+            countLabel.Content = controller.PersonCount;
+            countIndex.Content = controller.PersonIndex;
+        }
+
+        private void dwnBtn_Click(object sender, RoutedEventArgs e)
+        {
+            controller.PrevPerson();
+
+            firstNameTxtbox.Text = controller.CurrentPerson.FirstName;
+            lastNameTxtbox.Text = controller.CurrentPerson.LastName;
+            ageTxtbox.Text = controller.CurrentPerson.Age.ToString();
+            numberTxtbox.Text = controller.CurrentPerson.TelephoneNr;
+
+            countLabel.Content = controller.PersonCount;
+            countIndex.Content = controller.PersonIndex;
+        }
+        private void numberTxtbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SetButton();
+        }
+        private void firstNameTxtbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SetButton();
+        }
+        private void lastNameTxtbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SetButton();
+        }
+        private void ageTxtbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SetButton();
+        }
+    }
+}
